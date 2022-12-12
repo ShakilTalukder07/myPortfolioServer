b216f1c51639c22f9c4ef3d45db07488a26a478b
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-12-12 09:12:28 +0600

    done

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1ndgjy2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
 async function run() {
     try {
         await client.connect();
